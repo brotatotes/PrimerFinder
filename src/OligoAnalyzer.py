12172e2b20875ad1700d82dcaabd980e0b648087
brotatotes <hao.awesome@gmail.com>
2017-09-02 20:56:44 -0400

    comments

--- src/OligoAnalyzer.py
+++ src/OligoAnalyzer.py
@@ -1,16 +1,16 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class OligoAnalyzer(object):
     def __init__(self):
-        self.chromedriver_location = "/Users/Eric/Documents/myStuff/PrimerFinder/chromedriver"
+        self.chromedriver_location = "/Users/Eric/Documents/myStuff/PrimerFinder/src/chromedriver"
         self.oligo_analyzer_link = "http://idtdna.com/calc/analyzer"
         self.browser = webdriver.Chrome(self.chromedriver_location)
         self.browser.get(self.oligo_analyzer_link)
 
+        # limits how long to wait for page to load
         self.tries = 10000
         self.temperature_dict = {}
 
@@ -20,10 +20,12 @@ class OligoAnalyzer(object):
         if DNA_string in self.temperature_dict:
             return self.temperature_dict[DNA_string]
 
+        # find the textbox and enter DNA string
         text_box = self.browser.find_element_by_xpath("//*[@id='OligoAnalyzer']/div[2]/div[1]/div/div[1]/div[1]/div/div/textarea")
         text_box.clear()
         text_box.send_keys(DNA_string)
 
+        # find and click the analyze button
         analyze_button = self.browser.find_element_by_xpath("//*[@id='analyze-button']")
         analyze_button.click()
 
@@ -33,6 +35,7 @@ class OligoAnalyzer(object):
 
         while t < self.tries and (not temp or not temp_val):
             try:
+                # try to get temperature information (only works if page finishes loading)
                 temp = WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='OAResults']/div/div[1]/div[3]/div/div/table/tbody/tr[5]/td[2]/span")))
                 temp_text = temp.text
                 temp_val = float(temp_text[:temp_text.find(' ºC')])
